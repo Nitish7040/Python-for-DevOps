980ae5e0d973a5f0babe756f079a2f2057926e0e
Nitish pandey <Nitish7040@users.noreply.github.com>
2026-01-22 04:41:52 +0530

    add codes

---
+++ Day-1/01App.py
@@ -0,0 +1,22 @@
+#  String Handling functions  :----
+
+arn = "arn:aws:iam::123456789012:role/S3AccessRole"
+print (arn.split("/") [1])
+# output -->  S3AccessRole
+
+
+Name = "nitish"
+print(Name.upper())
+#  Output -->  NITISH
+
+
+str1= "Hello"
+str2= "Nitish"
+print(str1 + " " +str2)
+# Output --->  Hello Nitish
+
+text="nklebflvik;sdfvfsbggf"
+print(len(text))
+# output ---> 21
+
+
